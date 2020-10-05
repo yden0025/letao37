b8148690ac7aad0c7c24dd119ce9db0e12689e18
yden0025 <yden0025@student.monash.edu>
2020-10-05 18:19:27 +1100

    login completed

---
+++ public/back/js/login.js
@@ -0,0 +1,74 @@
+$(function () {
+  $("#form").bootstrapValidator({
+    excluded: [":disabled", ":hidden", ":not(:visible)"],
+    feedbackIcons: {
+      valid: "glyphicon glyphicon-ok",
+      invalid: "glyphicon glyphicon-remove",
+      validating: "glyphicon glyphicon-refresh",
+    },
+    fields: {
+      username: {
+        validators: {
+          notEmpty: {
+            message: "用户名不能为空",
+          },
+          stringLength: {
+            min: 2,
+            max: 6,
+            message: "用户名长度2-6位",
+          },
+          callback: {
+            message: "用户名不存在!",
+          },
+        },
+      },
+      password: {
+        validators: {
+          notEmpty: {
+            message: "密码不能为空",
+          },
+          stringLength: {
+            min: 6,
+            max: 12,
+            message: "用户名长度6-12位",
+          },
+          callback: {
+            message: "密码错误!",
+          },
+        },
+      },
+    },
+  });
+});
+
+$(".btn-reset").click(function () {
+  $(form).data("bootstrapValidator").resetForm();
+});
+
+$("#form").on("success.form.bv", function (e) {
+  e.preventDefault();
+  //使用ajax提交逻辑
+  $.ajax({
+    type: "post",
+    url: "/employee/employeeLogin",
+    data: $("#form").serialize(),
+    dataType: "json",
+    success: function (info) {
+      console.log(info);
+      if (info.error) {
+        if (info.error == 1000) {
+          $("#form")
+            .data("bootstrapValidator")
+            .updateStatus("username", "INVALID", "callback");
+        }
+        if (info.error == 1001) {
+          $("#form")
+            .data("bootstrapValidator")
+            .updateStatus("password", "INVALID", "callback");
+        }
+      } else {
+        location.href = "./index.html";
+      }
+    },
+  });
+});
